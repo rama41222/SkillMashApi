028d32d152cd433ebace99758385d1d38f7acf6a
rama41222 <dinushankanrg@gmail.com>
2018-03-31 02:38:53 +0530

    .

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,17 +4,6 @@
  * Last Modified: 3/31/18 2:25 AM by  Rama41222
  */
 
-const devConfig = {
-    MONGO_URL: 'mongodb://localhost/skillmash-dev',
-    JWT_SECRET: 'df#$EFDT$^$%&*(YUHGFYR^THF%$ETRFDfdrt56t5$N3cRH`',
-    FACEBOOK_SECRET:'f7624dfdaaa399e073a16e9b69cbe1ca'
-}
-
-const prodConfig = {
-    MONGO_URL: 'mongodb://localhost/skillmash-prod',
-    JWT_SECRET: '[k.|zO\'1Pdf#$EFDT$^$%&*(YUHGFYR^THF%$ETRFDfdrt56t5$N3cRH&oCA~t_!`pz=u-`byQ+)v:+^N3cRH`',
-    FACEBOOK_SECRET:'f7624dfdaaa399e073a16e9b69cbe1ca'
-}
 
 const defaultConfig = {
     port: process.env.port || 3410,
